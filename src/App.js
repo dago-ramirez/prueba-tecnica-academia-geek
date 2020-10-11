f9de17cbd50ff36b9024561424a9c01390893b3e
Dago R <dagoramirezserna@gmail.com>
2020-10-11 07:59:58 -0500

    Se crea el proyecto y se instala tailwind css2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-green-600" >Hola Mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
