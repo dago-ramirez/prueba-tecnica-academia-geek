501e6145296fa53e3f640da7b085b7d15a157797
Dago R <dagoramirezserna@gmail.com>
2020-10-13 14:20:15 -0500

    Se renderizan los componentes comentados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react';
 import { CookiesProvider } from 'react-cookie';
-// import Header from './components/Header';
-// import Registro from './components/Registro';
+import Header from './components/Header';
+import Registro from './components/Registro';
 import Repositorios from './components/Repositorios';
 
 export const DataContext = createContext(null);
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       <CookiesProvider >
         <DataContext.Provider value={{ data, setData }} >
-          {/* <Header />
-          <Registro /> */}
+          <Header />
+          <Registro />
           <Repositorios />
         </DataContext.Provider>
       </CookiesProvider>
---
+++ src/components/Paginacion.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export default function Paginacion({ totalRepos, showRepos}) {
+  if( totalRepos > 5 ) {
+    return (
+      <div className="w-full bg-green-700 px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+        {/*  sm:hidden */}
+        <div className="flex-1 flex justify-between">
+          <a href="/" id="previous" onClick={showRepos} className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-white bg-green-700 hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+            Previous
+          </a>
+          <a href="/" id="next" onClick={showRepos} className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-white bg-green-700 hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+            Next
+          </a>
+        </div>
+        <div className="hidden">
+        {/* <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between"> */}
+          <div>
+            <p className="text-md leading-5 text-white mr-6">
+              Total            
+              <span className="font-bold mx-1">{totalRepos}</span>
+              Repositorios
+            </p>
+          </div>
+          <div>
+            <nav className="relative z-0 inline-flex shadow-sm">
+              <a href="/" className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="Previous">
+                {/* <!-- Heroicon name: chevron-left --> */}
+                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+                </svg>
+              </a>
+              <a href="/" className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                1
+              </a>
+              <a href="/" className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                2
+              </a>
+              <a href="/" className="-ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                3
+              </a>
+              <a href="/" className="-ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                4
+              </a>
+              <a href="/" className="-ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                5
+              </a>            
+              <a href="/" className="-ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="Next">
+                {/* <!-- Heroicon name: chevron-right --> */}
+                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                </svg>
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  return <></>
+}
